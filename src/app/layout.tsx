5b1244fd615038d06cb7a65fedbf8a499d3792a0
Nandita <95146588+NanditaRK@users.noreply.github.com>
2024-07-07 21:59:06 -0700

    Add files via upload

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Elder Ease",
+  description: "Elder Ease is a web app that hopes to help the elderly manage their medical life by simplifying the process of booking medical appointments and managing their medications for them.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
