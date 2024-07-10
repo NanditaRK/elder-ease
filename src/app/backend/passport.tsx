f82acd2875f3bdaa81756a619e86a5a6195aa75a
Nandita Raj Kumar <nanditark@Nanditas-MacBook-Pro.local>
2024-07-09 20:59:33 -0700

    passport implementation

---
+++ src/app/backend/passport.tsx
@@ -0,0 +1,33 @@
+// backend/passport.js
+export {};
+
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const User = require('./models/User');
+
+passport.use(new GoogleStrategy({
+  clientID: 'YOUR_GOOGLE_CLIENT_ID',
+  clientSecret: 'YOUR_GOOGLE_CLIENT_SECRET',
+  callbackURL: '/auth/google/callback'
+}, async (accessToken, refreshToken, profile, done) => {
+  const existingUser = await User.findOne({ googleId: profile.id });
+  if (existingUser) {
+    return done(null, existingUser);
+  }
+  const newUser = new User({
+    googleId: profile.id,
+    name: profile.displayName,
+    email: profile.emails[0].value,
+  });
+  await newUser.save();
+  done(null, newUser);
+}));
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  const user = await User.findById(id);
+  done(null, user);
+});
